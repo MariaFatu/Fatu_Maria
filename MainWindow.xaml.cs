11292e6026ce9d0f03e5936f5a66d6eb3315175a
MariaFatu <maria.fatu@gmail.com>
2020-10-22 20:26:47 +0300

    Am creat DoughtnutMachine+more

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,10 +20,55 @@ namespace Fatu_Maria_Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int mRaisedGlazed;
+        private int mRaisedSugar;
+        private int mFilledLemon;
+        private int mFilledChocolate;
+        private int mFilledVanilla;
+
+        private DoughnutMachine myDoughnutMachine;
         public MainWindow()
         {
+
             InitializeComponent();
         }
+        private void DoughnutCompleteHandler()
+        {
+                switch (myDoughnutMachine.Flavor)
+                {
+                    case DoughnutType.Glazed:
+                        mRaisedGlazed++;
+                        txtGlazedRaised.Text = mRaisedGlazed.ToString();
+                        break;
+
+                    case DoughnutType.Sugar:
+                        mRaisedSugar++;
+                        txtSugarRaised.Text = mRaisedSugar.ToString();
+                        break;
+                }
+        }
+
+            private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine = new DoughnutMachine();
+            myDoughnutMachine.DoughnutComplete += new
+            DoughnutMachine.DoughnutCompleteDelegate(DoughnutCompleteHandler);
+        }
+
+        private void sugarToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = false;
+            sugarToolStripMenuItem.IsChecked = true;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Sugar);
+        }
+
+        private void glazedToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            glazedToolStripMenuItem.IsChecked = true;
+            sugarToolStripMenuItem.IsChecked = false;
+            myDoughnutMachine.MakeDoughnuts(DoughnutType.Glazed);
+        }
+    
 
         private void txtSugarRaised_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -31,6 +76,33 @@ namespace Fatu_Maria_Lab2
         }
 
         private void txtChocolateFilled_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+       
+        private void stopToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myDoughnutMachine.Enabled = false;
+        }
+        
+
+        
+        private void exitToolStripMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void txtQuantity_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (!(e.Key >= Key.D0 && e.Key <= Key.D9))
+            {
+                MessageBox.Show("Numai cifre se pot introduce!", "Input Error", MessageBoxButton.OK,
+               MessageBoxImage.Error);
+            }
+        }
+
+        private void txtGlezedRaised_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
